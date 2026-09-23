3abe81581380674eaa9cb27660f42feaf22d8561
zaibnawaz245-commits <zaibnawaz245@gmail.com>
2026-09-23 23:48:33 +0500

    Create q2_name.py

---
+++ q2_name.py
@@ -0,0 +1,8 @@
+# Question No 2 - Student Name Details
+name = input("Enter student name: ")
+
+print(f"Name in UPPERCASE: {name.upper()}")
+print(f"Name in lowercase: {name.lower()}")
+print(f"Number of characters: {len(name)}")
+print(f"First character: {name[0]}")
+print(f"Last character: {name[-1]}")
